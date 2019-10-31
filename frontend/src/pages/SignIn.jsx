c99b43375bb5de3353792cdcf4e77613259cc36c
Yoshihiro Kimura <kimuraysp@gmail.com>
2019-10-31 22:06:05 +0900

    state準備

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from "react"
 
 export const SignIn = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const submit = () => {
+    alert(`
+      ${email}
+      ${password}
+    `)
+  }
+
   return (
-    <form onSubmit={() => alert("Hi!")}>
+    <form onSubmit={submit}>
       <div>
         <label>メールアドレス</label>
-        <input type="email" />
+        <input type="email" onChange={(e) => setEmail(e.target.value)} />
       </div>
       <div>
         <label>パスワード</label>
-        <input type="password" />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
       </div>
       <div>
         <input type="submit" value="ログイン" />
