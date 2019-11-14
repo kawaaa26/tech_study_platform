7d5343e6fcc89c3fdf1d47b7319554141a0dd012
kawaaa26 <unic.almj@yahoo.com>
2019-11-14 22:10:38 +0900

    :construction: WIP: Authのresponseを確認中
    async/awaitを使用して検証模索

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,15 +1,28 @@
 import React, { useState } from "react"
+// import firebaseApp from "../lib/firebaseApp/index.js"
+import "firebase/auth"
+import * as firebaseApp from "../lib/firebaseApp/index.js"
+
+console.log(firebaseApp);
 
 export const SignIn = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
-  const submit = () => {
-    alert(`
+  const submit = async() => {
+       alert(`
       ${email}
       ${password}
     `)
-  }
+
+  // (async () => {
+
+    // const res = await firebaseApp.services_.auth().signInWithEmailAndPassword(email, password);
+    const res = await firebaseApp.auth().signInWithEmailAndPassword(email, password);
+    // firebase.auth().signInWithEmailAndPassword(email, password).then(function(res) {
+      console.log(res);
+    };
+
 
   return (
     <form onSubmit={submit}>
