46f1e1004d28cc5689809dcec2f804321fac64dd
kawaaa26 <unic.almj@yahoo.com>
2019-11-15 20:14:17 +0900

    :construction: WIP: firebaseUI experimenting...
    firebaseui-auth-containerを入れてみた consoleでのエラーが減った

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from "react"
-// import firebaseApp from "../lib/firebaseApp/index.js"
-import "firebase/auth"
 import * as firebaseApp from "../lib/firebaseApp/index.js"
-import * as firebase from 'firebase'
-import * as firebaseui from 'firebaseui'
-
+import * as firebase from "firebase"
+import * as firebaseui from "firebaseui"
 
 console.log(firebaseApp);
 
@@ -24,21 +21,22 @@ export const SignIn = () => {
     // const res = await firebaseApp.auth().signInWithEmailAndPassword(email, password);
     const ui = new firebaseui.auth.AuthUI(firebase.auth());
     // firebase.auth().signInWithEmailAndPassword(email, password).then(function(res) {
-      console.log(ui);
-    };
+    console.log(ui)
+    ui.start("#firebaseui-auth-container", submit)
+    }
 
 
   return (
     <form onSubmit={submit}>
-      <div>
+      <div id="firebaseui-auth-container">
         <label>メールアドレス</label>
         <input type="email" onChange={e => setEmail(e.target.value)} />
       </div>
-      <div>
+      <div id="firebaseui-auth-container">
         <label>パスワード</label>
         <input type="password" onChange={e => setPassword(e.target.value)} />
       </div>
-      <div>
+      <div id="firebaseui-auth-container">
         <input type="submit" value="ログイン" />
       </div>
     </form>
