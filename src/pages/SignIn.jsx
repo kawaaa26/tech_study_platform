19252bc0bb583a08eea02250b5bde14da9a144a5
kawaaa26 <unic.almj@yahoo.com>
2019-11-19 21:58:34 +0900

    :zap: firebaseから返ってきたuidを使えるか確認した
    使えそう
    
    Closes #8

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+import { firebaseApp } from "../lib/firebaseApp/index.js"
+
+console.log(firebaseApp)
+
+export const SignIn = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const submit = async () => {
+    alert(`
+      ${email}
+      ${password}
+    `)
+
+    try {
+      const res = await firebaseApp.auth().signInWithEmailAndPassword(email, password)
+      console.log("SUCCEEDED!")
+      console.log(res)
+
+      // uid使用できるか確認
+      const uid = res.user.uid
+      console.log(uid)
+    } catch (error) {
+      console.log(error)
+      console.log("Login Failed")
+    }
+  }
+
+  return (
+    <form onSubmit={submit}>
+      <div id="firebaseui-auth-container">
+        <label>メールアドレス</label>
+        <input type="email" onChange={e => setEmail(e.target.value)} />
+      </div>
+      <div id="firebaseui-auth-container">
+        <label>パスワード</label>
+        <input type="password" onChange={e => setPassword(e.target.value)} />
+      </div>
+      <div id="firebaseui-auth-container">
+        <input type="submit" value="ログイン" />
+      </div>
+    </form>
+  )
+}
